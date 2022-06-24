5b2324fcae5c2e6d2ef9421729e4a9d29a8a4033
PizzaLovingNerd <pizzalovingnerd@teknik.io>
2022-06-23 22:21:54 -0700

    Holy shit I actually finished this.

--- __main__.py
+++ __main__.py
@@ -2,21 +2,53 @@ import os
 import shutil
 import sys
 
-import css_config
-
-HOME_ = os.path.expanduser('~')
+import functions
 
 
-def reset():
-    os.remove(css_config.CSS_FILE_)
+def help_prompt():
+    print("""adwcolor: Quickly and easily customize Libadwaita's look
+
+Basic Management
+  adwcolor reset                                Restore default Libadwaita theme.
+  adwcolor install <path/libadwaita_theme.css>  Replaces ~/.config/gtk-4.0/gtk.css with Libadwaita css file.
+  adwcolor export <path/libadwaita_theme.css>   Export Libadwaita css file (creates clone of ~/.config/gtk-4.0/gtk.css)
+
+Modifying Current Theme
+  adwcolor listproperties                       Lists available properties to modify.
+  adwcolor modify <property> <value>            Set property to css color. (ex: "adwcolor modify accent_color @green_4")
+  adwcolor restore <property>                   Restore modified property to default. (ex: "adwcolor restore accent_color)
+    """)
 
 
-def install(file):
-    os.remove(css_config.CSS_FILE_)
-    shutil.copyfile(file, css_config.CSS_FILE_)
+if len(sys.argv) < 2:
+    help_prompt()
+    exit(1)
 
 
-def export(file):
-    shutil.copyfile(css_config.CSS_FILE_, file)
+def check_arguments(arg):
+    if not len(sys.argv) - 1 == arg:
+        help_prompt()
+        exit(1)
 
 
+match sys.argv[1]:
+    case "reset":
+        functions.reset()
+    case "install":
+        check_arguments(2)
+        functions.install(sys.argv[2])
+    case "export":
+        check_arguments(2)
+        functions.export(sys.argv[2])
+    case "modify":
+        check_arguments(3)
+        functions.check_properties(sys.argv[2])
+        functions.modify(sys.argv[2], sys.argv[3])
+    case "restore":
+        check_arguments(2)
+        functions.check_properties(sys.argv[2])
+        functions.restore(sys.argv[2])
+    case "listproperties":
+        functions.list_properties()
+    case default:
+        help_prompt()
---
+++ functions.py
@@ -0,0 +1,68 @@
+import os
+import shutil
+from properties import properties
+
+HOME_ = os.path.expanduser('~')
+CSS_FILE_ = f"{HOME_}/.config/gtk-4.0/gtk.css"
+
+
+def get_file_data():
+    with open(CSS_FILE_, "r") as file:
+        return file.readlines()
+
+
+def write(data):
+    with open(CSS_FILE_, "w") as file:
+        file.writelines(data)
+
+
+def modify(prop, value):
+    lines = get_file_data()
+    changed = False
+    new_lines = []
+
+    for line in lines:
+        if line.startswith(f"@define-color {prop} "):
+            line = f"@define-color {prop} {value};\n"
+            changed = True
+
+        new_lines.append(line)
+    if not changed:
+        new_lines.append(f"@define-color {prop} {value};\n")
+
+    write(new_lines)
+
+
+def restore(prop):
+    lines = get_file_data()
+    new_lines = []
+
+    for line in lines:
+        if not line.startswith(f"@define-color {prop}"):
+            new_lines.append(line)
+    write(new_lines)
+
+
+def reset():
+    if os.path.exists(CSS_FILE_):
+        os.remove(CSS_FILE_)
+
+
+def install(file):
+    reset()
+    shutil.copyfile(file, CSS_FILE_)
+
+
+def export(file):
+    shutil.copyfile(CSS_FILE_, file)
+
+
+def check_properties(prop):
+    if prop not in properties:
+        print("Invalid property.\nPlease run \"adwcolor listproperties\" to list currently existing properties.")
+        exit(1)
+
+
+def list_properties():
+    for prop in properties:
+        print(prop)
